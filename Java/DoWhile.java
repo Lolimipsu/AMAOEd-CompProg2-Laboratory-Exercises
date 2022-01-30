11d01130fca2c23dd63647760f769fd4dfb9fcf2
MxLoli the Outcast <44487404+Lolimipsu@users.noreply.github.com>
2022-01-30 13:50:56 +0800

    format DoWhile

--- Java/DoWhile.java
+++ Java/DoWhile.java
@@ -1,18 +1,16 @@
 import java.util.Scanner;
-public class DoWhile
-{
-	public static void main(String[] args) 
-    {
-		String name;
+
+public class DoWhile {
+    public static void main(String[] args) {
+        String name;
         int answer = 0;
         Scanner scan = new Scanner(System.in);
-        do{
+        do {
             System.out.println("Please input name:");
             name = scan.next();
             System.out.println("Hello Mr. " + name);
             System.out.println("would you like to change your name? insert 1 for yes and 2 for no.");
             answer = scan.nextInt();
-        }
-        while(answer == 1);
+        } while (answer == 1);
     }
 }
