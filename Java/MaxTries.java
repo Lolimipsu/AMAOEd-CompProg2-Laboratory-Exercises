1be158a3af5b04c2de290eac086a098dac8e5c86
MxLoli the Outcast <44487404+Lolimipsu@users.noreply.github.com>
2022-01-30 13:50:29 +0800

    Create MaxTries.java

---
+++ Java/MaxTries.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class MaxTries
+{
+	public static void main(String[] args) 
+    {
+        int atm_pin;
+        int max_tries = 0;
+        Scanner scan = new Scanner(System.in);
+        do{
+            System.out.println("Please input PIN:");
+            atm_pin = scan.nextInt();
+
+            if(atm_pin == 1234){
+                System.out.println("Nice ATM pin lol");
+                max_tries += 5;
+            }
+            else{
+                System.out.println("try again!\n");
+                max_tries += 1;
+            }
+        }
+        while(max_tries < 5);
+    }
+}
