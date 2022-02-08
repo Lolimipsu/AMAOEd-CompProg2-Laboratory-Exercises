aceec2aa8d2723dfc35a2c6b990c5ecb605d448e
MxLoli the Outcast <44487404+Lolimipsu@users.noreply.github.com>
2022-02-08 18:40:53 +0800

    implementation of AiO is complete

--- Java/AiO.java
+++ Java/AiO.java
@@ -1,71 +1,78 @@
 import java.util.Scanner;
 import java.util.Arrays;
+import java.io.IOException;
 
 // All in ONE (AiO)
 public class AiO
 {
     ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     // Main menu
-    public static void main(String[] args) 
+    public static void main(String[] args)throws IOException, InterruptedException 
     {
         // Definitions
         Scanner input = new Scanner(System.in);
-    
-        System.out.println("Which program would you like to run today?");
-        System.out.println("1 - Do While Loop\n2 - Greater than or Less than Element Finder\n3 - Max Tries\nInput:");
-        int menu_choice = input.nextInt();
+        int run_again = 0;
 
-        switch (menu_choice)
+        while (run_again !=2)
         {
-            case 1:
-                String name;
-                int answer = 0;
-                do {
-                    System.out.println("Please input name:");
-                    name = input.next();
-                    System.out.println("Hello Mr. " + name);
-                    System.out.println("would you like to change your name? insert 1 for yes and 2 for no.");
-                    answer = input.nextInt();
-                } while (answer == 1);
-                break;
+            System.out.println("Which program would you like to run today?");
+            System.out.println("1 - Do While Loop\n2 - Greater than or Less than Element Finder\n3 - Max Tries\nInput:");
+            int menu_choice = input.nextInt();
 
-            case 2:
-                int input_size;
-                // ask the user input to the amount of input they want.
-                System.out.println("How many inputs?");
-                input_size = input.nextInt();
-                // declares that the array element size is the same as the value of input_size
-                int array_elements[] = new int[input_size];
-                System.out.println("Enter " + input_size + " Array elements:");
-                for (int i = 0; i < array_elements.length; i++) {
-                    array_elements[i] = input.nextInt();
-                }
-                input.close();
+            switch (menu_choice)
+            {
+                case 1:
+                    String name;
+                    int answer = 0;
+                    do {
+                        System.out.println("Please input name:");
+                        name = input.next();
+                        System.out.println("Hello Mr. " + name);
+                        System.out.println("would you like to change your name? insert 1 for yes and 2 for no.");
+                        answer = input.nextInt();
+                    } while (answer == 1);
+                    break;
 
-                System.out.println("These are the numbers you have entered.");
-                printArray(array_elements);
+                case 2:
+                    int input_size;
+                    // ask the user input to the amount of input they want.
+                    System.out.println("How many inputs?");
+                    input_size = input.nextInt();
+                    // declares that the array element size is the same as the value of input_size
+                    int array_elements[] = new int[input_size];
+                    System.out.println("Enter " + input_size + " Array elements:");
+                    for (int i = 0; i < array_elements.length; i++) {
+                        array_elements[i] = input.nextInt();
+                    }
 
-                System.out.println("\nThis prints the greater number");
-                int n = array_elements.length;
+                    System.out.println("These are the numbers you have entered.");
+                    printArray(array_elements);
 
-                numChecker(array_elements, n);
-                for (int i = 0; i < n; i++) {
-                    System.out.print(array_elements[i] + ", ");
-                }
-                break;
+                    System.out.println("\nThis prints the greater number");
+                    int n = array_elements.length;
 
-            case 3:
-                
-                break;
+                    numChecker(array_elements, n);
+                    for (int i = 0; i < n; i++) {
+                        System.out.print(array_elements[i] + ", ");
+                    }
+                    break;
 
-        } // switch
+                case 3:
+                    
+                    break;
+
+            } // switch
+        System.out.println("\nWould you like to run the program again?\n1 - YES\t2 - NO");
+        run_again = input.nextInt();
+        Clear_Screen();
+        } // while loop
 
     } // main
     
     ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     // Greater than or Less than Element Finder
     ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-    // DEBUG PRINT
+    // #1 DEBUG PRINT
     public static void printArray(int array_elements[]) {
         int n = array_elements.length;
         for (int i = 0; i < n; i++) {
@@ -90,6 +97,11 @@ public class AiO
         }
         for (int i = 0; i < n; i++)
             array_elements[i] = temporary_Array[i];
+    }
+    // CLEAR SCREEN
+    public static void Clear_Screen()throws IOException, InterruptedException
+    {
+        new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();  
     }
     
 } // public class AiO
