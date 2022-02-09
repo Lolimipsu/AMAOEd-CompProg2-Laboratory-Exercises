b6bf566abc612c71b9b8ec379f4aa54f4f00a828
MxLoli the Outcast <44487404+Lolimipsu@users.noreply.github.com>
2022-02-09 13:34:00 +0800

    Update AiO.java
    program completed

--- Java/AiO.java
+++ Java/AiO.java
@@ -54,7 +54,27 @@ public class AiO {
                     break;
 
                 case 3:
-
+                    int attempts = 5,
+                        atm_pin = 1234,
+                        user_pin = 0;
+            
+                    do {
+                        System.out.print("Input your ATM PIN:\n");
+                        user_pin =input.nextInt();
+            
+                        if (user_pin != atm_pin) {
+                            attempts -= 1;
+                            if (attempts == 0) {
+                                System.out.println("No more attempts left");
+                            } else {
+                                System.out.println("Wrong PIN, Please try again.");
+                                System.out.println("You have " + attempts + " attempts left.");
+                            }
+                        } else {
+                            attempts = 0;
+                            System.out.println("\nNice ATM Pin lol\nYou have input the correct ATM PIN\nProgram is exiting now...");
+                        }
+                    } while (attempts != 0);
                     break;
 
                 default:
