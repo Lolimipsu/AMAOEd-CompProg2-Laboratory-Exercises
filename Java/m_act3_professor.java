0a6efab442f6996bd3c6fe5bcb52158d39f9adc9
MxLoli the Outcast <44487404+Lolimipsu@users.noreply.github.com>
2022-02-28 15:31:01 +0800

    Create m_act3_professor.java

---
+++ Java/m_act3_professor.java
@@ -0,0 +1,15 @@
+class Uni_Professor{
+    String name;
+    int salary;
+}
+
+class inherit_prof extends Uni_Professor{
+    String department_type;
+    String supply;
+
+}
+
+//main
+public class m_act3_professor{
+
+} 
