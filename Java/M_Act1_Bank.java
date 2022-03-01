1fbc98418c155cb133035deb5a944ba0ccea1aef
MxLoli the Outcast <44487404+Lolimipsu@users.noreply.github.com>
2022-03-01 10:18:30 +0800

    Update M_Act1_Bank.java

--- Java/M_Act1_Bank.java
+++ Java/M_Act1_Bank.java
@@ -1,34 +1,31 @@
 import java.util.Scanner;
 import java.io.IOException;
 
-public class M_Act1_Bank
-{
-    public static void main(String[] args) throws IOException, InterruptedException
-    {
-        //definitions
+public class M_Act1_Bank {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        // definitions
         int balance;
         int user_choice;
         Scanner input = new Scanner(System.in);
         int run_again = 0;
 
-        while (run_again !=2)
-        {
-            System.out.println("Input balance:");
+        while (run_again != 2) {
             balance = input.nextInt();
             System.out.println("-------------------------------------------------------");
             System.out.println("Select an option:\n1 - Withdraw\n2 - Deposit\n3 - Exit");
             user_choice = input.nextInt();
             System.out.println("-------------------------------------------------------");
-            switch (user_choice)
-            {
+            switch (user_choice) {
                 case 1:
                     withdraw(balance, input);
-                    run_app_again(run_again, input);
+                    System.out.println("\n\nRun program again?\n1 = YES\n2 = NO");
+                    run_again = input.nextInt();
                     cls();
                     break;
                 case 2:
                     deposit(balance, input);
-                    run_app_again(run_again, input);
+                    System.out.println("\n\nRun program again?\n1 = YES\n2 = NO");
+                    run_again = input.nextInt();
                     cls();
                     break;
                 case 3:
@@ -36,17 +33,16 @@ public class M_Act1_Bank
                     System.out.println("thanks for using my app");
                     run_again = 2;
                     break;
-            
+
                 default:
                     System.out.println("Please choose a valid option");
                     break;
-            }//switch
-        }//while
-    }//main
-    
-    //Withdraw
-    private static void withdraw(int balance, Scanner input)
-    {
+            }// switch
+        } // while
+    }// main
+
+    // Withdraw
+    private static void withdraw(int balance, Scanner input) {
         int withdraw_amt;
         int new_withdraw_bal;
         System.out.println("Withdraw amount:");
@@ -57,9 +53,8 @@ public class M_Act1_Bank
 
     }
 
-    //Deposit
-    private static void deposit(int balance, Scanner input)
-    {
+    // Deposit
+    private static void deposit(int balance, Scanner input) {
         int deposit_amt;
         int new_deposit_bal;
         System.out.println("Deposit amount:");
@@ -70,15 +65,7 @@ public class M_Act1_Bank
     }
 
     // Clear Screen
-    private static void cls() throws IOException, InterruptedException
-    {
+    private static void cls() throws IOException, InterruptedException {
         new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
     }
-
-    // run again
-    private static void run_app_again(int run_again, Scanner input)
-    {
-        System.out.println("\n\nRun program again?\n1 = YES\n2 = NO");
-        run_again = input.nextInt();
-    }
-}//public class Act1_Bank
+}// public class Act1_Bank
